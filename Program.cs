762dacf6d149d4cef6da3020318bf461ba4fdeb7
Burak Ates <imburakates@gmail.com>
2026-01-17 23:06:58 -0500

    Web API working

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ app.MapControllers();
 
 
 app.MapGet("/", () => Results.Content(
-    "<h1>Hello World</h1><p>Your Web API is running</p>",
+    "<h1>Hello World</h1><p>Web API is running</p>",
     "text/html"
 ));
 
